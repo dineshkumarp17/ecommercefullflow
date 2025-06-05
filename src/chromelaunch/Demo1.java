541d14579d488b48fefddd420750dbc773c2815e
dineshkumarp17 <dineshkumarcyr178@gmail.com>
2025-06-05 19:14:47 +0530

    First Commit

---
+++ src/chromelaunch/Demo1.java
@@ -0,0 +1,15 @@
+package chromelaunch;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:/Driver/chromedriver.exe");
+
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+	}
+
+}
